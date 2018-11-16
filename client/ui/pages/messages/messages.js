e0529bfca29ded9690f18e1eee61d29d5cb54595
thomas <thomas.michaluszko@gmail.com>
2018-11-16 12:41:47 +0100

    css

--- client/ui/pages/messages/messages.js
+++ client/ui/pages/messages/messages.js
@@ -1,26 +1,23 @@
-Template.message.onCreated(function () {
-
-});
+Template.message.onCreated(function() {});
 
 Template.messages.helpers({
-    messagesPrive() {
-        let routerUser = Router.current().params.userId;
-        let userId = Meteor.userId()
-        let sender = Newmsg.find({
-            sender: userId
-        }).fetch();
-        let reciever = Newmsg.find({
-            sender: routerUser,
-            reciever: userId
-        }).fetch();
+  messagePrive() {
+    let routerUser = Router.current().params.userId;
+    let userId = Meteor.userId();
+    let sender = Newmsg.find({
+      sender: userId
+    }).fetch();
+    let reciever = Newmsg.find({
+      sender: routerUser,
+      reciever: userId
+    }).fetch();
 
-        let allMessage = sender.concat(reciever);
-        return allMessage.sort((a, b) => {
-            return a.date - b.date
-        })
-    },
-    userId() {
-
-        return Router.current().params.userId;
-    }
+    let allMessage = sender.concat(reciever);
+    return allMessage.sort((a, b) => {
+      return a.date - b.date;
+    });
+  },
+  userId() {
+    return Router.current().params.userId;
+  }
 });
